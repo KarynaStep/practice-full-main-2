bc45622b1199854596235f8ab8916e2c056d53fb
KarynaStep <stepanova.carina98@gmail.com>
2023-12-06 10:48:28 +0200

    Refactor model Bank

--- server/src/models/Bank.js
+++ server/src/models/Bank.js
@@ -1,30 +1,48 @@
-
-
+'use strict';
+const {
+  Model
+} = require('sequelize');
 module.exports = (sequelize, DataTypes) => {
-  return sequelize.define('Banks', {
-    cardNumber: {
-      type: DataTypes.STRING,
-      allowNull: false,
-      primaryKey: true,
+  class Bank extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  }
+  Bank.init(
+    {
+      cardNumber: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        primaryKey: true,
+      },
+      name: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      expiry: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      cvc: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      balance: {
+        type: DataTypes.DECIMAL,
+        allowNull: false,
+        defaultValue: 0,
+      },
     },
-    name: {
-      type: DataTypes.STRING,
-      allowNull: false,
-    },
-    expiry: {
-      type: DataTypes.STRING,
-      allowNull: false,
-    },
-    cvc: {
-      type: DataTypes.STRING,
-      allowNull: false,
-    },
-    balance: {
-      type: DataTypes.DECIMAL,
-      allowNull: false,
-      defaultValue: 0,
-    },
-  }, {
-    timestamps: false,
-  });
+    {
+      sequelize,
+      modelName: 'Bank',
+      timestamps: true
+    }
+  );
+  return Bank;
 };
